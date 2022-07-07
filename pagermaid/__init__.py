a399759b47cf09945e1342b53e321353dd863aa7
xtaodada <xtao@xtaolink.cn>
2022-07-07 15:39:33 +0800

    实现 错误处理 钩子

--- pagermaid/__init__.py
+++ pagermaid/__init__.py
@@ -22,7 +22,7 @@ working_dir = getcwd()
 read_context = {}
 help_messages = {}
 hook_functions: Dict[str, Set[Callable[[], Awaitable[None]]]] = {
-    "startup": set(), "shutdown": set(), "command_pre": set(), "command_post": set()}
+    "startup": set(), "shutdown": set(), "command_pre": set(), "command_post": set(), "process_error": set(), }
 all_permissions = []
 
 logs = getLogger(__name__)
