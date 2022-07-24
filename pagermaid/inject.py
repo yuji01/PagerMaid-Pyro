57c826b06ec862af9b3b3ef7cc27cfcaecb9b013
xtaodada <xtao@xtaolink.cn>
2022-07-24 13:46:29 +0800

    ♻️ refactor: 依赖注入

---
+++ pagermaid/inject.py
@@ -0,0 +1,22 @@
+import inspect
+import pagermaid.enums as enums
+import pagermaid.services as services
+from typing import Dict, Optional
+
+
+def inject(message: enums.Message, function) -> Optional[Dict]:
+    try:
+        signature = inspect.signature(function)
+    except Exception:
+        return None
+    data = {}
+    for parameter_name, parameter in signature.parameters.items():
+        class_name = parameter.annotation.__name__
+        param = message if class_name == "Message" else services.get(class_name)
+        if not param:
+            if parameter_name == "message":
+                param = message
+            else:
+                param = services.get(parameter_name.capitalize())
+        data.setdefault(parameter_name, param)
+    return data
